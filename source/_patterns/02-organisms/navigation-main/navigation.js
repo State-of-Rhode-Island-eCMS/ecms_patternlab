711de8a0f0864ec19e4f30fca5922889c5383ce5
jhogue <highchairdesignhaus@gmail.com>
2020-10-21 14:46:52 -0400

    No hyphens in JS vars

--- source/_patterns/02-organisms/navigation-main/navigation.js
+++ source/_patterns/02-organisms/navigation-main/navigation.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Listen to mouse press, spacebar key press, and enter key press
-  var qh-dd-btns = document.querySelectorAll('.js-qh-dd-toggle');
-  qh-dd-btns.forEach(function(btn) {
+  var qh_dd_btns = document.querySelectorAll('.js-qh-dd-toggle');
+  qh_dd_btns.forEach(function(btn) {
     // a11yClick function restricts keypress to spacebar or enter
     if (a11yClick(e) === true) {
-      btn.addEventListener('click keypress', function (e) {
+      btn.addEventListener('click', function (e) {
         var expanded = btn.getAttribute('aria-expanded');
         // Close all
         btns.forEach(function(btn) {
