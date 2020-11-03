108cf012ffc47ad0398bd035f82e29c641ffaa38
Jason Pamental <jason@rwt.io>
2020-11-03 09:25:35 -0500

    Subsetting fonts, setting up font loading

--- dist/js/pattern-lab-compiled.js
+++ dist/js/pattern-lab-compiled.js
@@ -23,6 +23,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  // Toggle button for mobile menu
+  var qh_toggle_btn = document.getElementById('js-toggle-nav');
+  qh_toggle_btn.addEventListener('click', function(event) {
+    // a11yClick function restricts keypress to spacebar or enter
+    if (a11yClick(event) === true) {
+      var expanded = qh_toggle_btn.getAttribute('aria-expanded');
+      if (expanded == 'true') {
+        qh_toggle_btn.setAttribute('aria-expanded', 'false');
+      } else {
+        qh_toggle_btn.setAttribute('aria-expanded', 'true');
+      }
+    }
+  });
+
   // Listen to mouse press, spacebar key press, and enter key press
   var qh_dd_btns = document.querySelectorAll('.js-qh-dd-toggle');
   qh_dd_btns.forEach(function(toggle) {
