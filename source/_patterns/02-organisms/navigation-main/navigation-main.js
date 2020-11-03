a43f303d61352288b5f631b9539b452706c8cd3a
jhogue <highchairdesignhaus@gmail.com>
2020-11-03 11:41:45 -0500

    Mobile menu progress
    • Positioning in place
    • Transitions/animations in place
    • Duplicate home button in place
    • Mobile menu button in place
    • Vertical right nav drawer styled and working

--- source/_patterns/02-organisms/navigation-main/navigation-main.js
+++ source/_patterns/02-organisms/navigation-main/navigation-main.js
@@ -2,17 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Toggle button for mobile menu
   var qh_toggle_btn = document.getElementById('js-toggle-nav');
+  var qh_body_element = document.getElementsByTagName('body')[0];
+  qh_body_element.classList.add('qh__nav-main__overlay');
   qh_toggle_btn.addEventListener('click', function(event) {
     // a11yClick function restricts keypress to spacebar or enter
     if (a11yClick(event) === true) {
       var expanded = qh_toggle_btn.getAttribute('aria-expanded');
       if (expanded == 'true') {
         qh_toggle_btn.setAttribute('aria-expanded', 'false');
+        qh_body_element.classList.remove('qh__nav-main__overlay--visible');
       } else {
         qh_toggle_btn.setAttribute('aria-expanded', 'true');
+        qh_body_element.classList.add('qh__nav-main__overlay--visible');
       }
     }
   });
+  // TODO: Add handler that checks for a click outside of Menu and closes the menu
+  // Since the body overlay is a ::after element, this needs to be done by processing the
+  // x y coordinates of the click and checking to see if it is inside the menu or not
+
 
   // Listen to mouse press, spacebar key press, and enter key press
   var qh_dd_btns = document.querySelectorAll('.js-qh-dd-toggle');
