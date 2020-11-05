7151314a805772151d42aef5e5264ce7aa03a97c
Jason Pamental <jason@rwt.io>
2020-11-05 08:35:30 -0500

    Nav toggle button background and color var fix

--- dist/js/pattern-lab-compiled.js
+++ dist/js/pattern-lab-compiled.js
@@ -21,7 +21,114 @@ document.addEventListener("DOMContentLoaded", function() {
   document.documentElement.classList.add("js");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+// There are custom properties that track what breakpoint the site is using
+function getQhNavState() {
+  // NOTE: Strings from CSS get returned exactly as written
+  // To check them in JS for true/false, they need to be written exactly
+  //
+  // Example:
+  //   --nav-state: 'mobile';
+  // Can be checked with ' \"mobile\"';
+  //   --nav-state:mobile;
+  // Can be checked with 'mobile';
+  //
+  // The strings are VERY literal
+  //
+  var qh_body = document.getElementsByTagName('body')[0];
+  return getComputedStyle(qh_body).getPropertyValue("--nav-state");
+}
+
+// Mobile: Move the Search component into the Navigation drawer
+// Benefit of insertBefore is that it moves the element, not a copy of the element
+function moveSearchIntoNav(direction) {
+  var qh_search = document.getElementById('js__site-search');
+  var qh_header = document.getElementById('js__search-social');
+  var qh_mainnav = document.getElementById('js__primary-menu');
+
+  if (qh_search !== null && qh_search !== undefined) {
+    // Which direction is this going?
+    if (direction === 'toNav') {
+      if (qh_mainnav !== null && qh_mainnav !== undefined) {
+        // Create a new <li>
+        var qh_new_li = document.createElement('li');
+        qh_new_li.classList.add('qh__nav-main__item__search');
+        // Add the HTML of the search inside of it
+        qh_new_li.appendChild(qh_search);
+        // insert before the first child
+        qh_mainnav.insertBefore(qh_new_li, qh_mainnav.firstChild);
+      }
+    } else {
+      if (qh_header !== null && qh_mainnav !== undefined) {
+        // Append search to the header container
+        qh_header.appendChild(qh_search);
+        // Grab the previous search LI we created
+        var qh_prev_li = document.getElementsByClassName('qh__nav-main__item__search');
+        if (qh_prev_li[0] !== null && qh_prev_li[0] !== undefined) {
+          qh_prev_li[0].parentNode.removeChild(qh_prev_li[0]);
+        }
+      }
+    }
+  }
+}
+
+// Mobile: Move the Social List component into the Navigation drawer
+function moveSocialIntoNav(direction) {
+  var qh_social = document.getElementById('js__social-links');
+  var qh_header = document.getElementById('js__search-social');
+  var qh_mainnav = document.getElementById('js__primary-menu');
+
+  if (qh_social !== null && qh_social !== undefined) {
+    // Which direction is this going?
+    if (direction === 'toNav') {
+      if (qh_mainnav !== null && qh_mainnav !== undefined) {
+        // Create a new <li>
+        var qh_new_li = document.createElement('li');
+        qh_new_li.classList.add('qh__nav-main__item__social');
+        // Add the HTML of the social list inside of it
+        qh_new_li.appendChild(qh_social);
+        // insert after the last child
+        qh_mainnav.insertBefore(qh_new_li, qh_mainnav.lastChild.nextSibling);
+      }
+    } else {
+      if (qh_header !== null && qh_header !== undefined) {
+        // Append search to the header container
+        qh_header.appendChild(qh_social);
+        // Grab the previous search LI we created
+        var qh_prev_li = document.getElementsByClassName('qh__nav-main__item__social');
+        if (qh_prev_li[0] !== null && qh_prev_li[0] !== undefined) {
+          qh_prev_li[0].parentNode.removeChild(qh_prev_li[0]);
+        }
+      }
+    }
+  }
+}
+
+function moveSearchAndSocial() {
+  var test = getQhNavState();
+  if (test.trim() === 'js-mobile') {
+    moveSearchIntoNav('toNav');
+    moveSocialIntoNav('toNav');
+  } else {
+    moveSocialIntoNav('fromNav');
+    moveSearchIntoNav('fromNav');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Call on page ready
+  moveSearchAndSocial();
+
+  // Call again on resize or orientation change
+  // With a debounced window resize handler
+  var qh_window_timeout = false;
+  window.addEventListener('resize', function() {
+    clearTimeout(qh_window_timeout);
+    qh_window_timeout = setTimeout(moveSearchAndSocial, 250);
+  });
+  window.addEventListener('orientationchange', moveSearchAndSocial);
+
 
   // Toggle button for mobile menu
   var qh_toggle_btn = document.getElementById('js-toggle-nav');
