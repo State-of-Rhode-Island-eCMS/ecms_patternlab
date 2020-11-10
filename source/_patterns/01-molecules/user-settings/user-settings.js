734dcf586e36575e656cd38e1c4d0defb197c95c
Jason Pamental <jason@rwt.io>
2020-11-10 15:38:32 -0500

    better JS formatting for user settings

--- source/_patterns/01-molecules/user-settings/user-settings.js
+++ source/_patterns/01-molecules/user-settings/user-settings.js
@@ -7,59 +7,67 @@ window.onload = function(){
 const settingsMenuToggle = document.getElementById('settings_trigger');
 
 const settingsMenuTriggers = [].slice.call(document.querySelectorAll('.settings-menu-trigger'));
-settingsMenuTriggers.forEach(settingsMenuTrigger => settingsMenuTrigger.addEventListener('click', function(e) {
-	e.preventDefault();
-	if (settingsMenuTrigger.parentElement.classList.contains('open')) {
-		settingsMenuTrigger.parentElement.classList.remove('open');
-		deactivatePageOverlay();
-	} else {
-		// reset open menus
-		allMenuCloser();
-		settingsMenuTrigger.parentElement.classList.add('open');
-		activatePageOverlay();
-	}
-}));
 
+if (settingsMenuTriggers !== null && settingsMenuTriggers !== undefined) {
+  settingsMenuTriggers.forEach(settingsMenuTrigger => settingsMenuTrigger.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (settingsMenuTrigger.parentElement.classList.contains('open')) {
+      settingsMenuTrigger.parentElement.classList.remove('open');
+      deactivatePageOverlay();
+    } else {
+      // reset open menus
+      allMenuCloser();
+      settingsMenuTrigger.parentElement.classList.add('open');
+      activatePageOverlay();
+    }
+  }));
+}
 
 // Light mode settings
 const lightModeToggle = document.getElementById('light_mode_switch');
 const lightModeReset = document.getElementById('light_mode_reset');
 var osLightMode = getComputedStyle(document.documentElement).getPropertyValue('--osLightMode').trim();
 
-lightModeToggle.addEventListener('click', function(e) {
-	e.preventDefault();
-	if (osLightMode == 'dark') {
-		// set a cookie to expire the setting
-		console.log('check: '+osLightMode);
-		document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
-		document.getElementsByTagName("html")[0].classList.remove('dark');
-		document.getElementsByTagName("html")[0].classList.add('light');
-	} else if (document.getElementsByTagName("html")[0].classList.contains('dark')) {
-		// set a cookie to save the setting
-		document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
-		document.getElementsByTagName("html")[0].classList.remove('dark');
-		document.getElementsByTagName("html")[0].classList.add('light');
-	} else {
-		// set a cookie to save the setting
-		document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
-		document.getElementsByTagName("html")[0].classList.remove('light');
-		document.getElementsByTagName("html")[0].classList.add('dark');
-	}
-	e.blur();
-});
-lightModeReset.addEventListener('click', function(e) {
-	e.preventDefault();
-	document.cookie = "lightMode=auto; max-age=31536000; path=/; samesite=strict";
-	document.getElementsByTagName("html")[0].classList.remove('dark');
-	document.getElementsByTagName("html")[0].classList.remove('light');
-	e.blur();
-});
+if (lightModeToggle !== null && lightModeToggle !== undefined) {
+  lightModeToggle.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (osLightMode == 'dark') {
+      // set a cookie to expire the setting
+      console.log('check: '+osLightMode);
+      document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
+      document.getElementsByTagName("html")[0].classList.remove('dark');
+      document.getElementsByTagName("html")[0].classList.add('light');
+    } else if (document.getElementsByTagName("html")[0].classList.contains('dark')) {
+      // set a cookie to save the setting
+      document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
+      document.getElementsByTagName("html")[0].classList.remove('dark');
+      document.getElementsByTagName("html")[0].classList.add('light');
+    } else {
+      // set a cookie to save the setting
+      document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
+      document.getElementsByTagName("html")[0].classList.remove('light');
+      document.getElementsByTagName("html")[0].classList.add('dark');
+    }
+    e.blur();
+  });
+}
 
+if (lightModeReset !== null && lightModeReset !== undefined) {
+  lightModeReset.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.cookie = "lightMode=auto; max-age=31536000; path=/; samesite=strict";
+    document.getElementsByTagName("html")[0].classList.remove('dark');
+    document.getElementsByTagName("html")[0].classList.remove('light');
+    e.blur();
+  });
+}
 // Font size settings 
 const fontSizeSlider = document.getElementById('font_size_modifier');
 var currentFontSizeModifier = getComputedStyle(document.documentElement).getPropertyValue('--fontSizeModifier');
 
-fontSizeSlider.addEventListener('change', handleFontSizeSliderUpdate);
+if (fontSizeSlider !== null && fontSizeSlider !== undefined) {
+  fontSizeSlider.addEventListener('change', handleFontSizeSliderUpdate);
+}
 
 function handleFontSizeSliderUpdate(e) {
 	document.documentElement.style.setProperty(`--fontSizeModifier`, this.value);
@@ -76,7 +84,9 @@ function fontSizeSliderSet() {
 const lineHeightSlider = document.getElementById('line_height_modifier');
 var currentlineHeightModifier = getComputedStyle(document.documentElement).getPropertyValue('--lineHeightModifier');
 
-lineHeightSlider.addEventListener('change', handleLineHeightSliderUpdate);
+if (lineHeightSlider !== null && lineHeightSlider !== undefined) {
+  lineHeightSlider.addEventListener('change', handleLineHeightSliderUpdate);
+}
 
 function handleLineHeightSliderUpdate(e) {
 	document.documentElement.style.setProperty(`--lineHeightModifier`, this.value);
@@ -93,7 +103,9 @@ function lineHeightSliderSet() {
 const wordSpaceSlider = document.getElementById('word_space_modifier');
 var currentWordSpaceModifier = getComputedStyle(document.documentElement).getPropertyValue('--wordSpaceModifier');
 
-wordSpaceSlider.addEventListener('change', handleWordSpaceSliderUpdate);
+if (wordSpaceSlider !== null && wordSpaceSlider !== undefined) {
+  wordSpaceSlider.addEventListener('change', handleWordSpaceSliderUpdate);
+}
 
 function handleWordSpaceSliderUpdate(e) {
 	document.documentElement.style.setProperty(`--wordSpaceModifier`, this.value);
