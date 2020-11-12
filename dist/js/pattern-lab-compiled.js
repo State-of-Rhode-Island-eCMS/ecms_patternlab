fc81d65d8c0a3fa15c5f55cb1c891c592fe474a5
Jason Pamental <jason@rwt.io>
2020-11-12 17:36:34 -0500

    Fixing ‘switch to light mode’ when dark OS pref

--- dist/js/pattern-lab-compiled.js
+++ dist/js/pattern-lab-compiled.js
@@ -415,6 +415,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Hide notifications based on cookie value.
+  var notificationCookie = getCookie('siteNotifications');
+  if (notificationCookie === 'hidden') {
+    console.log('hidden');
+    document.getElementById('summary-notifications').setAttribute('aria-expanded', 'false');
+    document.getElementById('details-notifications').classList.remove('js__aria-expanded');
+  }
+
+  // Add logic to set cookie values based on state of button.
+  const notificationsToggle = document.getElementById('summary-notifications');
+  notificationsToggle.addEventListener('click', function(event) {
+    if (a11yClick(event) === true) {
+      var expanded = notificationsToggle.getAttribute('aria-expanded');
+
+      console.log('clicked')
+      if (expanded == 'true') {
+        document.cookie = "siteNotifications=display; max-age=31536000; path=/; samesite=strict";
+      } else {
+        document.cookie = "siteNotifications=hidden; max-age=31536000; path=/; samesite=strict";
+      }
+    }
+  })
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Toggle button for mobile menu
@@ -442,29 +468,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Hide notifications based on cookie value.
-  var notificationCookie = getCookie('siteNotifications');
-  if (notificationCookie === 'hidden') {
-    console.log('hidden');
-    document.getElementById('summary-notifications').setAttribute('aria-expanded', 'false');
-    document.getElementById('details-notifications').classList.remove('js__aria-expanded');
-  }
-
-  // Add logic to set cookie values based on state of button.
-  const notificationsToggle = document.getElementById('summary-notifications');
-  notificationsToggle.addEventListener('click', function(event) {
-    if (a11yClick(event) === true) {
-      var expanded = notificationsToggle.getAttribute('aria-expanded');
-
-      console.log('clicked')
-      if (expanded == 'true') {
-        document.cookie = "siteNotifications=display; max-age=31536000; path=/; samesite=strict";
-      } else {
-        document.cookie = "siteNotifications=hidden; max-age=31536000; path=/; samesite=strict";
-      }
-    }
-  })
-});
-
