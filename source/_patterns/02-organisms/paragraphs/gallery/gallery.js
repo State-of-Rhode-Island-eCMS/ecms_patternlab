22d61034dc21e73ad080a7caf7176e713e815ef5
Jordan Caldwell <jcaldwell@oomphinc.com>
2020-12-09 14:22:04 -0500

    First commit of gallery

---
+++ source/_patterns/02-organisms/paragraphs/gallery/gallery.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var slider = tns({
+    container: '.qh__gallery__slider',
+    items: 1,
+    controlsContainer: ".qh__gallery__controls",
+    navContainer: ".qh__gallery__nav",
+    mouseDrag: true,
+    loop: true,
+  });
+});
