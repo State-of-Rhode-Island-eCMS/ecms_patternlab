86f8246ea63e0000f8cadad88b341f873272f2a9
Jordan Caldwell <jcaldwell@oomphinc.com>
2020-12-10 12:18:19 -0500

    WIP gallery js

--- source/_patterns/02-organisms/paragraphs/gallery/gallery.js
+++ source/_patterns/02-organisms/paragraphs/gallery/gallery.js
@@ -1,11 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-  var slider = tns({
-    container: '.qh__gallery__slider',
-    items: 1,
-    controlsContainer: ".qh__gallery__controls",
-    navContainer: ".qh__gallery__nav",
-    mouseDrag: true,
-    loop: true,
-  });
-});
+//   var slider = tns({
+//     container: '.qh__gallery__slider',
+//     items: 1,
+//     controlsContainer: ".qh__gallery__controls",
+//     navContainer: ".qh__gallery__nav",
+//     mouseDrag: true,
+//     loop: true,
+//   });
+// });
+
+
+var checkReadyState = setInterval(() => {
+  if (document.readyState === "complete") {
+    clearInterval(checkReadyState);
+
+    var slider = tns({
+      container: '.qh__gallery__slider',
+      items: 1,
+      controlsContainer: ".qh__gallery__controls",
+      navContainer: ".qh__gallery__nav",
+      mouseDrag: true,
+      loop: true,
+    });
+  }
+}, 100);
