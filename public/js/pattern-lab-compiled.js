646622350d3002323cfe583045d5c40650e05a23
“kbeck303” <kbeck303@gmail.com>
2024-08-06 14:49:50 -0400

    Add styles and JS for google_translator languages

--- public/js/pattern-lab-compiled.js
+++ public/js/pattern-lab-compiled.js
@@ -42,6 +42,12 @@ function allMenuCloser() {
   if (qh_usersettings_btn !== null) {
     qh_usersettings_btn.setAttribute('aria-expanded', 'false');
   }
+
+  // Close language nav
+  var qh_userlanguage_btn = document.getElementById('js__user-language__toggle');
+  if (qh_userlanguage_btn !== null) {
+    qh_userlanguage_btn.setAttribute('aria-expanded', 'false');
+  }
 }
 
 // Add screen overlay
@@ -1223,8 +1229,8 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
       }
       return b;
     }
-    var ct = function ct() {
-        return ct._nlp || (ct._nlp = n(3)), ct._nlp;
+    var _ct = function ct() {
+        return _ct._nlp || (_ct._nlp = n(3)), _ct._nlp;
       },
       ht = {
         MO: new b.b(0),
@@ -1307,9 +1313,9 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
       this.options = n;
     }
     var ft = (lt.parseText = function (t, e) {
-      return ct().parseText(t, e);
+      return _ct().parseText(t, e);
     }, lt.fromText = function (t, e) {
-      return ct().fromText(t, e);
+      return _ct().fromText(t, e);
     }, lt.fromString = function (t) {
       return new lt(lt.parseString(t) || void 0);
     }, lt.prototype._iter = function (t) {
@@ -1353,9 +1359,9 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
     }, lt.prototype.toString = function () {
       return R(this.origOptions);
     }, lt.prototype.toText = function (t, e, n) {
-      return ct().toText(this, t, e, n);
+      return _ct().toText(this, t, e, n);
     }, lt.prototype.isFullyConvertibleToText = function () {
-      return ct().isFullyConvertible(this);
+      return _ct().isFullyConvertible(this);
     }, lt.prototype.clone = function () {
       return new lt(this.origOptions);
     }, lt.FREQUENCIES = ["YEARLY", "MONTHLY", "WEEKLY", "DAILY", "HOURLY", "MINUTELY", "SECONDLY"], lt.YEARLY = y.YEARLY, lt.MONTHLY = y.MONTHLY, lt.WEEKLY = y.WEEKLY, lt.DAILY = y.DAILY, lt.HOURLY = y.HOURLY, lt.MINUTELY = y.MINUTELY, lt.SECONDLY = y.SECONDLY, lt.MO = ht.MO, lt.TU = ht.TU, lt.WE = ht.WE, lt.TH = ht.TH, lt.FR = ht.FR, lt.SA = ht.SA, lt.SU = ht.SU, lt.parseString = O, lt.optionsToString = R, lt);
@@ -2298,7 +2304,7 @@ var tns = function () {
       }
     };
   }
-  var aa = function aa(O) {
+  var _aa = function aa(O) {
     O = Hi({
       container: ".slider",
       mode: "carousel",
@@ -2789,7 +2795,7 @@ var tns = function () {
           U = !1;
         },
         rebuild: function rebuild() {
-          return aa(Hi(O, E));
+          return _aa(Hi(O, E));
         }
       };
     }
@@ -3449,8 +3455,175 @@ var tns = function () {
     }
     M && console.warn("No slides found in", O.container);
   };
-  return aa;
+  return _aa;
 }();
+"use strict";
+
+//rrule/dist/es5/rrule.min.js
+document.addEventListener("DOMContentLoaded", function () {
+  var rules = document.querySelectorAll('.qh__teaser-event__time-rrule');
+  _forEach(rules, function (index, value) {
+    var rrule_string = value.dataset.rrule;
+    if (window.rrule && rrule_string) {
+      var humanText = window.rrule.rrulestr(rrule_string).toText();
+      value.innerHTML = humanText;
+    }
+  });
+});
+"use strict";
+
+window.onload = function () {
+  var vfSupport = false;
+  var vfSupport = "CSS" in window && "supports" in CSS && CSS.supports("(font-variation-settings: normal)");
+  if (vfSupport === true) {
+    fontSizeSliderSet();
+    lineHeightSliderSet();
+    wordSpaceSliderSet();
+  } else {
+    if (document.getElementById('qh-fontsize')) {
+      document.getElementById('qh-fontsize').style.display = 'none';
+    }
+    if (document.getElementById('qh-lineheight')) {
+      document.getElementById('qh-lineheight').style.display = 'none';
+    }
+    if (document.getElementById('qh-wordspace')) {
+      document.getElementById('qh-wordspace').style.display = 'none';
+    }
+  }
+};
+function handleFontSizeSliderUpdate(e) {
+  document.documentElement.style.setProperty("--fontSizeModifier", this.value);
+  document.cookie = "fontSizeModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
+}
+function fontSizeSliderSet() {
+  var fontSizeModifier = getComputedStyle(document.documentElement).getPropertyValue('--fontSizeModifier');
+  var fontSizeElement = document.getElementById('font_size_modifier');
+  if (fontSizeElement !== null && fontSizeElement !== undefined) {
+    fontSizeElement.setAttribute('value', fontSizeModifier.trim());
+  }
+}
+function handleLineHeightSliderUpdate(e) {
+  document.documentElement.style.setProperty("--lineHeightModifier", this.value);
+  document.cookie = "lineHeightModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
+}
+function lineHeightSliderSet() {
+  var lineHeightModifier = getComputedStyle(document.documentElement).getPropertyValue('--lineHeightModifier');
+  var lineSpaceElement = document.getElementById('line_height_modifier');
+  if (lineSpaceElement !== null && lineSpaceElement !== undefined) {
+    lineSpaceElement.setAttribute('value', lineHeightModifier.trim());
+  }
+}
+function handleWordSpaceSliderUpdate(e) {
+  document.documentElement.style.setProperty("--wordSpaceModifier", this.value);
+  document.cookie = "wordSpaceModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
+}
+function wordSpaceSliderSet() {
+  var wordSpaceModifier = getComputedStyle(document.documentElement).getPropertyValue('--wordSpaceModifier');
+  var wordSpaceElement = document.getElementById('word_space_modifier');
+  if (wordSpaceElement !== null && wordSpaceElement !== undefined) {
+    wordSpaceElement.setAttribute('value', wordSpaceModifier.trim());
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  var qh_usersettings_btn = document.getElementById('js__user-settings__toggle');
+  //console.log('qh_usersettings_btn: ' + qh_usersettings_btn);
+  if (qh_usersettings_btn !== null && qh_usersettings_btn !== undefined) {
+    //console.log('qh_usersettings_btn is not null or undefined');
+    qh_usersettings_btn.addEventListener('click', function (event) {
+      // a11yClick function restricts keypress to spacebar or enter
+      if (a11yClick(event) === true) {
+        var expanded = qh_usersettings_btn.getAttribute('aria-expanded');
+        if (expanded == 'true') {
+          qh_usersettings_btn.setAttribute('aria-expanded', 'false');
+          deactivatePageOverlay();
+        } else {
+          allMenuCloser();
+          qh_usersettings_btn.setAttribute('aria-expanded', 'true');
+          activatePageOverlay();
+        }
+      }
+    });
+  }
+  var qh_userlanguage_btn = document.getElementById('js__user-language__toggle');
+  //console.log('qh_userlanguage_btn: ' + qh_userlanguage_btn);
+  if (qh_userlanguage_btn !== null && qh_userlanguage_btn !== undefined) {
+    //console.log('qh_userlanguage_btn is not null or undefined');
+    qh_userlanguage_btn.addEventListener('click', function (event) {
+      // a11yClick function restricts keypress to spacebar or enter
+      if (a11yClick(event) === true) {
+        var expanded = qh_userlanguage_btn.getAttribute('aria-expanded');
+        if (expanded == 'true') {
+          qh_userlanguage_btn.setAttribute('aria-expanded', 'false');
+          deactivatePageOverlay();
+        } else {
+          allMenuCloser();
+          qh_userlanguage_btn.setAttribute('aria-expanded', 'true');
+          activatePageOverlay();
+        }
+      }
+    });
+  }
+
+  // Light mode settings
+  var lightModeToggle = document.getElementById('light_mode_switch');
+  var lightModeReset = document.getElementById('light_mode_reset');
+  var lightModeCookie = getCookie('lightMode');
+
+  // If using automatic cookie, set body class so toggle works
+
+  if (lightModeToggle !== null && lightModeToggle !== undefined) {
+    lightModeToggle.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Always check the value of the custom property before determining state.
+      if (getComputedStyle(document.documentElement).getPropertyValue('--osLightMode').trim() == 'dark' && !document.getElementsByTagName("html")[0].classList.contains('light')) {
+        document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
+        document.getElementsByTagName("html")[0].classList.remove('dark');
+        document.getElementsByTagName("html")[0].classList.add('light');
+      } else if (getComputedStyle(document.documentElement).getPropertyValue('--osLightMode').trim() == 'light' && document.getElementsByTagName("html")[0].classList.contains('dark')) {
+        document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
+        document.getElementsByTagName("html")[0].classList.remove('dark');
+        document.getElementsByTagName("html")[0].classList.add('light');
+      } else {
+        // set a cookie to save the setting
+        document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
+        document.getElementsByTagName("html")[0].classList.remove('light');
+        document.getElementsByTagName("html")[0].classList.add('dark');
+      }
+    });
+  }
+  if (lightModeReset !== null && lightModeReset !== undefined) {
+    lightModeReset.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.cookie = "lightMode=auto; max-age=31536000; path=/; samesite=strict";
+
+      // Remove any current body classes.
+      document.getElementsByTagName("html")[0].classList.remove('dark');
+      document.getElementsByTagName("html")[0].classList.remove('light');
+    });
+  }
+
+  // Font size settings
+  var fontSizeSlider = document.getElementById('font_size_modifier');
+  var currentFontSizeModifier = getComputedStyle(document.documentElement).getPropertyValue('--fontSizeModifier');
+  if (fontSizeSlider !== null && fontSizeSlider !== undefined) {
+    fontSizeSlider.addEventListener('change', handleFontSizeSliderUpdate);
+  }
+
+  // Line-height settings
+  var lineHeightSlider = document.getElementById('line_height_modifier');
+  var currentlineHeightModifier = getComputedStyle(document.documentElement).getPropertyValue('--lineHeightModifier');
+  if (lineHeightSlider !== null && lineHeightSlider !== undefined) {
+    lineHeightSlider.addEventListener('change', handleLineHeightSliderUpdate);
+  }
+
+  // Word space settings
+  var wordSpaceSlider = document.getElementById('word_space_modifier');
+  var currentWordSpaceModifier = getComputedStyle(document.documentElement).getPropertyValue('--wordSpaceModifier');
+  if (wordSpaceSlider !== null && wordSpaceSlider !== undefined) {
+    wordSpaceSlider.addEventListener('change', handleWordSpaceSliderUpdate);
+  }
+});
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -3739,154 +3912,6 @@ if (notificationsToggle !== null && notificationsToggle !== undefined) {
     }
   });
 }
-"use strict";
-
-//rrule/dist/es5/rrule.min.js
-document.addEventListener("DOMContentLoaded", function () {
-  var rules = document.querySelectorAll('.qh__teaser-event__time-rrule');
-  _forEach(rules, function (index, value) {
-    var rrule_string = value.dataset.rrule;
-    if (window.rrule && rrule_string) {
-      var humanText = window.rrule.rrulestr(rrule_string).toText();
-      value.innerHTML = humanText;
-    }
-  });
-});
-"use strict";
-
-window.onload = function () {
-  var vfSupport = false;
-  var vfSupport = "CSS" in window && "supports" in CSS && CSS.supports("(font-variation-settings: normal)");
-  if (vfSupport === true) {
-    fontSizeSliderSet();
-    lineHeightSliderSet();
-    wordSpaceSliderSet();
-  } else {
-    if (document.getElementById('qh-fontsize')) {
-      document.getElementById('qh-fontsize').style.display = 'none';
-    }
-    if (document.getElementById('qh-lineheight')) {
-      document.getElementById('qh-lineheight').style.display = 'none';
-    }
-    if (document.getElementById('qh-wordspace')) {
-      document.getElementById('qh-wordspace').style.display = 'none';
-    }
-  }
-};
-function handleFontSizeSliderUpdate(e) {
-  document.documentElement.style.setProperty("--fontSizeModifier", this.value);
-  document.cookie = "fontSizeModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
-}
-function fontSizeSliderSet() {
-  var fontSizeModifier = getComputedStyle(document.documentElement).getPropertyValue('--fontSizeModifier');
-  var fontSizeElement = document.getElementById('font_size_modifier');
-  if (fontSizeElement !== null && fontSizeElement !== undefined) {
-    fontSizeElement.setAttribute('value', fontSizeModifier.trim());
-  }
-}
-function handleLineHeightSliderUpdate(e) {
-  document.documentElement.style.setProperty("--lineHeightModifier", this.value);
-  document.cookie = "lineHeightModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
-}
-function lineHeightSliderSet() {
-  var lineHeightModifier = getComputedStyle(document.documentElement).getPropertyValue('--lineHeightModifier');
-  var lineSpaceElement = document.getElementById('line_height_modifier');
-  if (lineSpaceElement !== null && lineSpaceElement !== undefined) {
-    lineSpaceElement.setAttribute('value', lineHeightModifier.trim());
-  }
-}
-function handleWordSpaceSliderUpdate(e) {
-  document.documentElement.style.setProperty("--wordSpaceModifier", this.value);
-  document.cookie = "wordSpaceModifier=" + this.value + "; max-age=31536000; path=/; samesite=strict";
-}
-function wordSpaceSliderSet() {
-  var wordSpaceModifier = getComputedStyle(document.documentElement).getPropertyValue('--wordSpaceModifier');
-  var wordSpaceElement = document.getElementById('word_space_modifier');
-  if (wordSpaceElement !== null && wordSpaceElement !== undefined) {
-    wordSpaceElement.setAttribute('value', wordSpaceModifier.trim());
-  }
-}
-document.addEventListener("DOMContentLoaded", function () {
-  var qh_usersettings_btn = document.getElementById('js__user-settings__toggle');
-  //console.log('qh_usersettings_btn: ' + qh_usersettings_btn);
-  if (qh_usersettings_btn !== null && qh_usersettings_btn !== undefined) {
-    //console.log('qh_usersettings_btn is not null or undefined');
-    qh_usersettings_btn.addEventListener('click', function (event) {
-      // a11yClick function restricts keypress to spacebar or enter
-      if (a11yClick(event) === true) {
-        var expanded = qh_usersettings_btn.getAttribute('aria-expanded');
-        if (expanded == 'true') {
-          qh_usersettings_btn.setAttribute('aria-expanded', 'false');
-          deactivatePageOverlay();
-        } else {
-          allMenuCloser();
-          qh_usersettings_btn.setAttribute('aria-expanded', 'true');
-          activatePageOverlay();
-        }
-      }
-    });
-  }
-
-  // Light mode settings
-  var lightModeToggle = document.getElementById('light_mode_switch');
-  var lightModeReset = document.getElementById('light_mode_reset');
-  var lightModeCookie = getCookie('lightMode');
-
-  // If using automatic cookie, set body class so toggle works
-
-  if (lightModeToggle !== null && lightModeToggle !== undefined) {
-    lightModeToggle.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      // Always check the value of the custom property before determining state.
-      if (getComputedStyle(document.documentElement).getPropertyValue('--osLightMode').trim() == 'dark' && !document.getElementsByTagName("html")[0].classList.contains('light')) {
-        document.cookie = "lightMode=light; max-age=31536000; path=/; samesite=strict";
-        document.getElementsByTagName("html")[0].classList.remove('dark');
-        document.getElementsByTagName("html")[0].classList.add('light');
-      } else if (getComputedStyle(document.documentElement).getPropertyValue('--osLightMode').trim() == 'light' && document.getElementsByTagName("html")[0].classList.contains('dark')) {
-        document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
-        document.getElementsByTagName("html")[0].classList.remove('dark');
-        document.getElementsByTagName("html")[0].classList.add('light');
-      } else {
-        // set a cookie to save the setting
-        document.cookie = "lightMode=dark; max-age=31536000; path=/; samesite=strict";
-        document.getElementsByTagName("html")[0].classList.remove('light');
-        document.getElementsByTagName("html")[0].classList.add('dark');
-      }
-    });
-  }
-  if (lightModeReset !== null && lightModeReset !== undefined) {
-    lightModeReset.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.cookie = "lightMode=auto; max-age=31536000; path=/; samesite=strict";
-
-      // Remove any current body classes.
-      document.getElementsByTagName("html")[0].classList.remove('dark');
-      document.getElementsByTagName("html")[0].classList.remove('light');
-    });
-  }
-
-  // Font size settings
-  var fontSizeSlider = document.getElementById('font_size_modifier');
-  var currentFontSizeModifier = getComputedStyle(document.documentElement).getPropertyValue('--fontSizeModifier');
-  if (fontSizeSlider !== null && fontSizeSlider !== undefined) {
-    fontSizeSlider.addEventListener('change', handleFontSizeSliderUpdate);
-  }
-
-  // Line-height settings
-  var lineHeightSlider = document.getElementById('line_height_modifier');
-  var currentlineHeightModifier = getComputedStyle(document.documentElement).getPropertyValue('--lineHeightModifier');
-  if (lineHeightSlider !== null && lineHeightSlider !== undefined) {
-    lineHeightSlider.addEventListener('change', handleLineHeightSliderUpdate);
-  }
-
-  // Word space settings
-  var wordSpaceSlider = document.getElementById('word_space_modifier');
-  var currentWordSpaceModifier = getComputedStyle(document.documentElement).getPropertyValue('--wordSpaceModifier');
-  if (wordSpaceSlider !== null && wordSpaceSlider !== undefined) {
-    wordSpaceSlider.addEventListener('change', handleWordSpaceSliderUpdate);
-  }
-});
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
